b2f0842895545070993310c3d502853d4c28d0e2
JacobAshmore <31559996+JacobAshmore@users.noreply.github.com>
2025-09-16 01:57:15 -0400

    Scenes Added
    Added options, linked options button and exit options buttons

--- scenes/options.gd
+++ scenes/options.gd
@@ -11,17 +11,17 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
+func _on_controls_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Options.tscn")
+func _on_video_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_audio_pressed() -> void:
+	pass # Replace with function body.
 
 
 func _on_exit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_join_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
