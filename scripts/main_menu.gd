cc535f887aebb569d614e74b7b8207bcb1df121f
JacobAshmore <31559996+JacobAshmore@users.noreply.github.com>
2025-09-16 01:37:11 -0400

    Update Start Button
    Made start button pass

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Levels/lab-escape.tscn")
+	pass # Replace with function body.
 
 
 func _on_options_pressed() -> void:
