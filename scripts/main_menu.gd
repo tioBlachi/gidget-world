c5769ea5329f2db52527f36b10921641d40ac493
JacobAshmore <31559996+JacobAshmore@users.noreply.github.com>
2025-09-20 02:16:06 -0400

    Merge Fix
    Merge Fix, caused by origin being old main

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/DevTools/Testing.tscn")
 
 
 func _on_options_pressed() -> void:
