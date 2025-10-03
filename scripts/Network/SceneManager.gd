b4c31b43681209e5991c732d92368fd426f52ae7
Collin Whitney <collinwhitney621@gmail.com>
2025-10-03 16:56:24 -0400

    Lawnmower mk2

--- scripts/Network/SceneManager.gd
+++ scripts/Network/SceneManager.gd
@@ -5,8 +5,7 @@ const SCENES: Dictionary = {
 	"Lab Escape": "res://scenes/Levels/lab-escape.tscn",
 	"Urban Uprising": "res://scenes/Levels/urban_uprising.tscn",
 	"Car Dodge": "res://scenes/Levels/carDodge.tscn",
-	"Sewer Climb": "res://scenes/Levels/sewer_climb.tscn",
-	"Lawnmower Madness": "res://scenes/Levels/lawnmower_madness.tscn",
+	"lawnmower madness": "res://scenes/Levels/lawnmower_madness.tscn",
 	"Title": "res://scenes/UI/Title.tscn",
 	"Testing" : "res://scenes/DevTools/Testing.tscn",
 	"Lobby" : "res://scenes/UI/Lobby.tscn"
