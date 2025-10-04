72cb4605f7eb0f65dec4a31b15130093ada7ddd6
JacobAshmore <31559996+JacobAshmore@users.noreply.github.com>
2025-10-04 01:19:24 -0400

    Update
    Update - Added debris scene,

--- scripts/Levels/falling_in_sewer.gd
+++ scripts/Levels/falling_in_sewer.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	for player in get_tree().get_nodes_in_group("players"):
+		player.is_gravity_level = true
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +14,9 @@ func _process(delta: float) -> void:
 
 	
 	pass
+
+
+
+func _on_death_line_body_entered(body: Node2D) -> void:
+	if body.is_in_group("players"):
+		body.die()
---
+++ scripts/objects/debris.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var gravity: float = 600.0
+@export var slow_multiplier: float = 0.4
+@export var max_fall_speed: float = 250.0
+
+#var velocity: Vector2 = Vector2.ZERO
+
+func _ready():
+	pass #$AnimatedSprite2D.play("fall") # play your falling animation
+
+func _physics_process(delta):
+	velocity.y += gravity * slow_multiplier * delta
+	velocity.y = min(velocity.y, max_fall_speed)
+	move_and_slide()
+
+	# Optional: remove when it goes off-screen
+	#if global_position.y > 1200: # adjust to match your level
+		#queue_free()
