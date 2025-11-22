d48b05aea1db55757b2be24745ab4142a7fcea22
Blas Antunez <blasantunez310@gmail.com>
2025-11-21 21:30:39 -0500

    Synched Sewer Climb for multiplayer and added Pause UI and level passed/failed popup

--- scripts/Levels/sewer_climb.gd
+++ scripts/Levels/sewer_climb.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
-@export var map_limits: Rect2 = Rect2(0, 0, 2048, 2048)
+@export var map_limits: Rect2 = Rect2(130.0, -198.0, 1500, 1100)
 @onready var player_scene = preload("res://scenes/player/player.tscn")
 @onready var pSpawner = $pSpawner
 @onready var player1marker = $PlayerMarkers/Player1Marker
 @onready var player2marker = $PlayerMarkers/Player2Marker
+@onready var popup = $PopupUI/restart_screen
+@onready var door = $LabExitDoor
+
+var players_in_game := Net.players.size()
 
 func _ready() -> void:
 	if multiplayer.is_server():
 		spawn_players.rpc(Net.players)
+	
+	door.player_left.connect( func():
+		players_in_game -= 1
+		check_win()
+		)
+		
+func check_win() -> void:
+	print("Checking for win...")
+	print(players_in_game)
+	if players_in_game <= 0:
+		popup.current_state = popup.LEVEL_STATE.COMPLETE
+		popup.pause()
 
 @rpc("authority", "call_local", "reliable")
 func spawn_players(p_array: PackedInt32Array) -> void:
